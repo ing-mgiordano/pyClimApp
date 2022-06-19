cec0c1a58f47fcd8a3615c0af71ec23db947a54f
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-19 20:12:35 +0200

    se crea formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,58 @@
 
 const Formulario = () => {
     return (
-        <div>
-            form
+        <div className="contenedor">
+            <form>
+                <div className="campo">
+                    <label htmlFor="ciudad">Ciudad</label>
+                    <input
+                        type="text"
+                        id="ciudad"
+                        name="ciudad"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="pais">País</label>
+                    <select
+                        name="pais"
+                        id="pais"
+                    >
+                        <option value="">-- Seleccione País --</option>
+                        <option value="DE">Alemania</option>
+                        <option value="AR">Argentina</option>
+                        <option value="AT">Austria</option>
+                        <option value="AU">Australia</option>
+                        <option value="BS">Bahamas</option>
+                        <option value="BO">Bolivia</option>
+                        <option value="BR">Brasil</option>
+                        <option value="CA">Canadá</option>
+                        <option value="CN">China</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="AE">Emiratos Árabes Unidos</option>
+                        <option value="ES">España</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="FR">Francia</option>
+                        <option value="FI">Findalindia</option>
+                        <option value="IT">Italia</option>
+                        <option value="JP">Japón</option>
+                        <option value="MX">México</option>
+                        <option value="NZ">Nueva Zelanda</option>
+                        <option value="PE">Perú</option>
+                        <option value="GB">Reino Unido</option>
+                        <option value="ZA">Sudáfrica</option>
+                        <option value="CH">Suiza</option>
+                        <option value="UA">Ucrania</option>
+                        <option value="UY">Uruguay</option>
+                    </select>
+                </div>
+
+                <input  
+                    type="submit" 
+                    value="Consultar Clima"    
+                />
+            </form>
         </div>
     )
 }
